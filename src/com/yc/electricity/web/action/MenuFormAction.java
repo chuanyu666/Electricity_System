04ea00c1c5d0fb162969905fa24ddf1d19b444d5
Chuan Yu <chuan@upnest.com>
2016-03-23 00:25:27 -0700

    Move jsp directory

---
+++ src/com/yc/electricity/web/action/MenuFormAction.java
@@ -0,0 +1,81 @@
+package com.yc.electricity.web.action;
+
+import com.yc.electricity.web.action.form.MenuForm;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+
+/**
+ * Created by Chuan on 3/22/16.
+ */
+@ParentPackage("struts-default")
+@Namespace(value = "/system")
+@Controller
+@Scope(value = "prototype")
+public class MenuFormAction extends BaseAction<MenuForm>{
+
+    private MenuForm menuForm = this.getModel();
+
+
+    @Action(value = "menuHome",results = {
+            @Result(name = SUCCESS, location = "/WEB-INF/page/menu/home.jsp")
+    })
+    public String menuHome() {
+        System.out.println(menuForm.getName() + "  " + menuForm.getPassword());
+        return SUCCESS;
+    }
+
+    @Action(value = "menuTitle",results = {
+            @Result(name = SUCCESS, location = "/WEB-INF/page/menu/title.jsp")
+    })
+    public String menuTitle() {
+        return SUCCESS;
+    }
+
+    @Action(value = "menuLeft",results = {
+            @Result(name = SUCCESS, location = "/WEB-INF/page/menu/left.jsp")
+    })
+    public String menuLeft() {
+        return SUCCESS;
+    }
+
+    @Action(value = "menuChange",results = {
+            @Result(name = SUCCESS, location = "/WEB-INF/page/menu/change.jsp")
+    })
+    public String menuChange() {
+        return SUCCESS;
+    }
+
+    @Action(value = "menuLoading",results = {
+            @Result(name = SUCCESS, location = "/WEB-INF/page/menu/loading.jsp")
+    })
+    public String menuLoading() {
+        return SUCCESS;
+    }
+
+    @Action(value = "alarmStation",results = {
+            @Result(name = SUCCESS, location = "/WEB-INF/page/menu/alarmStation.jsp")
+    })
+    public String alarmStation() {
+        return SUCCESS;
+    }
+
+    @Action(value = "alarmDevice",results = {
+            @Result(name = SUCCESS, location = "/WEB-INF/page/menu/alarmDevice.jsp")
+    })
+    public String alarmDevice() {
+        return SUCCESS;
+    }
+
+    @Action(value = "logout",results = {
+            @Result(name = SUCCESS, type = "redirect", location = "/index.jsp")
+    })
+    public String logout() {
+        request.getSession().invalidate();
+        return SUCCESS;
+    }
+}
---
+++ src/com/yc/electricity/web/action/form/MenuForm.java
@@ -0,0 +1,26 @@
+package com.yc.electricity.web.action.form;
+
+/**
+ * Created by Chuan on 3/22/16.
+ */
+public class MenuForm {
+
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
